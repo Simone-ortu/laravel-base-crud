9020668aff21297c8f9cd11e7b4355abe9995805
Shinnolo <simo.28.09o@gmai.com>
2021-03-17 17:07:10 +0100

    Show

--- app/Http/Controllers/DressController.php
+++ app/Http/Controllers/DressController.php
@@ -13,7 +13,12 @@ class DressController extends Controller
      */
     public function index()
     {
-        return view('dressed.index');
+        $vestiti = Dress::all();
+
+        $data= [
+            'vestiti' => $vestiti
+        ];
+        return view('dressed.index', $data);
     }
 
     /**
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,3 +3,24 @@
 @section('title','home vestiti')
 
 @section('content')
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Id</th>
+      <th scope="col">Marca</th>
+      <th scope="col">Modello</th>
+      <th scope="col">Taglia</th>
+    </tr>
+  </thead>
+  <tbody>
+  @foreach($vestiti as $vestito)
+    <tr>
+      <th scope="row">{{$vestito->id}}</th>
+      <td>{{$vestito->Marca}}</td>
+      <td>{{$vestito->Modello}}</td>
+      <td>{{$vestito->Taglia}}</td>
+      <td><a href="{{route(dresses.show , ['dress' => $vestito->id])}}">Dettagli</a></td>
+    </tr>
+  </tbody>
+</table>
+@endsection
